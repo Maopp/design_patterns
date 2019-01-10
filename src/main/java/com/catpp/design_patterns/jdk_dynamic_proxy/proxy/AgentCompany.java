bd9db9f1ae920e3254a8cf64096ee866615ba07c
Maopp <1121266440@qq.com>
2019-01-10 10:14:38 +0800

    JDK动态代理

---
+++ src/main/java/com/catpp/design_patterns/jdk_dynamic_proxy/proxy/AgentCompany.java
@@ -0,0 +1,40 @@
+package com.catpp.design_patterns.jdk_dynamic_proxy.proxy;
+
+import com.catpp.design_patterns.jdk_dynamic_proxy.service.FemaleArtist;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * com.catpp.design_patterns.jdk_dynamic_proxy.proxy
+ *
+ * @Author cat_pp
+ * @Date 2019/1/10
+ * @Description 经纪人公司，代理艺人活动
+ */
+public class AgentCompany implements InvocationHandler {
+
+    protected FemaleArtist femaleArtist;
+
+    public AgentCompany(FemaleArtist femaleArtist) {
+        this.femaleArtist = femaleArtist;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 约会之前
+        beforeDating();
+        Object invoke = method.invoke(femaleArtist, args);
+        // 约会之后
+        afterDating();
+        return invoke;
+    }
+
+    private void afterDating() {
+        System.err.println("约会之前要打扮一下哦。。。");
+    }
+
+    private void beforeDating() {
+        System.err.println("约会之后洗个澡吧。。。");
+    }
+}
---
+++ src/main/java/com/catpp/design_patterns/jdk_dynamic_proxy/service/FemaleArtist.java
@@ -0,0 +1,19 @@
+package com.catpp.design_patterns.jdk_dynamic_proxy.service;
+
+/**
+ * com.catpp.design_patterns.jdk_dynamic_proxy
+ *
+ * @Author cat_pp
+ * @Date 2019/1/10
+ * @Description 代理接口
+ */
+public interface FemaleArtist {
+
+    /**
+     * 约会接口
+     *
+     * @param height 身高
+     * @return 是否同意约会
+     */
+    boolean dating(float height);
+}
