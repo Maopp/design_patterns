856df1ef9bb155e0754253822b703b974b46021f
Maopp <1121266440@qq.com>
2019-01-16 15:45:14 +0800

    建造者模式

---
+++ src/main/java/com/catpp/design_patterns/builder_pattern/item/impl/ColdDrink.java
@@ -0,0 +1,22 @@
+package com.catpp.design_patterns.builder_pattern.item.impl;
+
+import com.catpp.design_patterns.builder_pattern.item.Item;
+import com.catpp.design_patterns.builder_pattern.packing.Packing;
+import com.catpp.design_patterns.builder_pattern.packing.impl.Bottle;
+
+/**
+ * com.catpp.design_patterns.builder_pattern.item.impl
+ *
+ * @Author cat_pp
+ * @Date 2019/1/16
+ * @Description
+ */
+public abstract class ColdDrink implements Item {
+    @Override
+    public String packing() {
+        return new Bottle().pack();
+    }
+
+    @Override
+    public abstract float price();
+}
