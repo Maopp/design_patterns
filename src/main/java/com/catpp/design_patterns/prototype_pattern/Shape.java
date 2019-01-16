95e2b0f653abbf956b053e41aa116db17e8cdb88
Maopp <1121266440@qq.com>
2019-01-16 17:15:27 +0800

    原型模式

---
+++ src/main/java/com/catpp/design_patterns/prototype_pattern/Shape.java
@@ -0,0 +1,35 @@
+package com.catpp.design_patterns.prototype_pattern;
+
+/**
+ * com.catpp.design_patterns.prototype_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/16
+ * @Description 接口类
+ */
+public abstract class Shape implements Cloneable {
+
+    private String id;
+    protected String type;
+
+    protected abstract void draw();
+
+    public String getType(){
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Object clone = null;
+        clone = super.clone();
+        return clone;
+    }
+}
---
+++ src/main/java/com/catpp/design_patterns/prototype_pattern/impl/Circle.java
@@ -0,0 +1,22 @@
+package com.catpp.design_patterns.prototype_pattern.impl;
+
+import com.catpp.design_patterns.prototype_pattern.Shape;
+
+/**
+ * com.catpp.design_patterns.prototype_pattern.impl
+ *
+ * @Author cat_pp
+ * @Date 2019/1/16
+ * @Description 实体类
+ */
+public class Circle extends Shape {
+
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    protected void draw() {
+        System.err.println("Inside Circle::draw() method.");
+    }
+}
