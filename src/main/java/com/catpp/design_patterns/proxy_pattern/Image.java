86eb6d57c4c94929b7aa71942fa780b6c59fd8b7
Maopp <1121266440@qq.com>
2019-01-21 18:04:05 +0800

    代理模式

---
+++ src/main/java/com/catpp/design_patterns/proxy_pattern/Image.java
@@ -0,0 +1,13 @@
+package com.catpp.design_patterns.proxy_pattern;
+
+/**
+ * com.catpp.design_patterns.proxy_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/21
+ * @Description
+ */
+public interface Image {
+
+    void display();
+}
