4562c63f6b14d3e1cfdce094f07ffc244d28ac89
Maopp <1121266440@qq.com>
2019-01-21 17:27:17 +0800

    享元模式

---
+++ src/main/java/com/catpp/design_patterns/fly_weight_pattern/Shape.java
@@ -0,0 +1,13 @@
+package com.catpp.design_patterns.fly_weight_pattern;
+
+/**
+ * com.catpp.design_patterns.fly_weight_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/21
+ * @Description
+ */
+public interface Shape {
+
+    void draw();
+}
