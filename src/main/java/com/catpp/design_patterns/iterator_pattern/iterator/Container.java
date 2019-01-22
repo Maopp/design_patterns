3e9b0ebb71a9ee1c1ce378ab4b4620f030b7b890
Maopp <1121266440@qq.com>
2019-01-22 17:12:44 +0800

    迭代器模式

---
+++ src/main/java/com/catpp/design_patterns/iterator_pattern/iterator/Container.java
@@ -0,0 +1,13 @@
+package com.catpp.design_patterns.iterator_pattern.iterator;
+
+/**
+ * com.catpp.design_patterns.iterator_pattern.iterator
+ *
+ * @Author cat_pp
+ * @Date 2019/1/22
+ * @Description
+ */
+public interface Container {
+
+    Iterator getIterator();
+}
