b2612c11b177f51c3f1d868198f40d8f792900e8
Maopp <1121266440@qq.com>
2019-01-22 19:35:13 +0800

    备忘录模式

---
+++ src/main/java/com/catpp/design_patterns/memento_pattern/CareTaker.java
@@ -0,0 +1,26 @@
+package com.catpp.design_patterns.memento_pattern;
+
+import com.catpp.design_patterns.memento_pattern.memento.Memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * com.catpp.design_patterns.memento_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/22
+ * @Description 从Memento恢复对象的状态
+ */
+public class CareTaker {
+
+    private List<Memento> mementos = new ArrayList<>();
+
+    public void add(Memento memento) {
+        mementos.add(memento);
+    }
+
+    public Memento get(int index) {
+        return mementos.get(index);
+    }
+}
---
+++ src/main/java/com/catpp/design_patterns/memento_pattern/Originator.java
@@ -0,0 +1,31 @@
+package com.catpp.design_patterns.memento_pattern;
+
+import com.catpp.design_patterns.memento_pattern.memento.Memento;
+
+/**
+ * com.catpp.design_patterns.memento_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/22
+ * @Description 创建备忘录，存储状态
+ */
+public class Originator {
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(state);
+    }
+
+    public void getStateFromMemento(Memento memento) {
+        this.state = memento.getState();
+    }
+}
