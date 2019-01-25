af8f8226ceae60ad9209f7a654e8d87852b580e5
Maopp <1121266440@qq.com>
2019-01-25 17:03:36 +0800

    组合实体模式

---
+++ src/main/java/com/catpp/design_patterns/composite_entity_pattern/composite_entity/CompositeEntity.java
@@ -0,0 +1,23 @@
+package com.catpp.design_patterns.composite_entity_pattern.composite_entity;
+
+import com.catpp.design_patterns.composite_entity_pattern.coarse_grained.CoarseGrainedObject;
+
+/**
+ * com.catpp.design_patterns.composite_entity_pattern.composite_entity
+ *
+ * @Author cat_pp
+ * @Date 2019/1/25
+ * @Description 组合实体类
+ */
+public class CompositeEntity {
+
+    private CoarseGrainedObject cgo = new CoarseGrainedObject();
+
+    public void setData(String data1, String data2){
+        cgo.setData(data1, data2);
+    }
+
+    public String[] getData(){
+        return cgo.getData();
+    }
+}
