83ae1efb39af1df2f5492b8591e96e08936454b8
Maopp <1121266440@qq.com>
2019-01-25 16:26:57 +0800

    业务代表模式

---
+++ src/main/java/com/catpp/design_patterns/business_delegate_pattern/BusinessService.java
@@ -0,0 +1,13 @@
+package com.catpp.design_patterns.business_delegate_pattern;
+
+/**
+ * com.catpp.design_patterns.business_delegate_pattern
+ *
+ * @Author cat_pp
+ * @Date 2019/1/25
+ * @Description
+ */
+public interface BusinessService {
+
+    void doProcessing();
+}
