afbd64ee06dc36ab30f7fbdf38d49b65e09ba800
Maopp <1121266440@qq.com>
2019-01-25 14:53:46 +0800

    MVC模式

--- README.md
+++ README.md
@@ -898,3 +898,17 @@ TemplatePatternDemo演示类使用 Game 来演示模板模式的用法。
 们将定义另一个接口 ComputerPartVisitor，它定义了访问者类的操作。Computer 使用实体访问者来执行相应的动作。
 VisitorPatternDemo演示类使用 Computer、ComputerPartVisitor 类来演示访问者模式的用法。
 ![访问者模式实例](/src/main/resources/images/访问者模式实例.jpg)
+
+### MVC模式：
+> MVC 模式代表 Model-View-Controller（模型-视图-控制器） 模式。这种模式用于应用程序的分层开发
+
+- **Model（模型）：** 模型代表一个存取数据的对象或 JAVA POJO。它也可以带有逻辑，在数据变化时更新控制器。
+- **View（视图）：** 视图代表模型包含的数据的可视化。
+- **Controller（控制器）：** 控制器作用于模型和视图上。它控制数据流向模型对象，并在数据变化时更新视图。它使视图与模型分离开。
+![MVC模式](/src/main/resources/images/MVC模式.png)
+
+#### 实现
+> 创建一个作为模型的 Student 对象。StudentView 是一个把学生详细信息输出到控制台的视图类，StudentController 是负责存储数
+据到 Student 对象中的控制器类，并相应地更新视图 StudentView。
+MVCPatternDemo演示类使用 StudentController 来演示 MVC 模式的用法。
+![MVC模式实例](/src/main/resources/images/MVC模式实例.jpg)
---
+++ src/main/java/com/catpp/design_patterns/mvc_pattern/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.catpp.design_patterns.mvc_pattern.controller;
+
+import com.catpp.design_patterns.mvc_pattern.model.Student;
+import com.catpp.design_patterns.mvc_pattern.view.StudentView;
+
+/**
+ * com.catpp.design_patterns.mvc_pattern.controller
+ *
+ * @Author cat_pp
+ * @Date 2019/1/25
+ * @Description
+ */
+public class StudentController {
+
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name){
+        model.setName(name);
+    }
+
+    public String getStudentName(){
+        return model.getName();
+    }
+
+    public void setStudentRollNo(String rollNo){
+        model.setRollNo(rollNo);
+    }
+
+    public String getStudentRollNo(){
+        return model.getRollNo();
+    }
+
+    public void updateView(){
+        view.printStudentDetails(model.getName(), model.getRollNo());
+    }
+}
